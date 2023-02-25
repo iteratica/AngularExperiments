e7a2dfe5cd873f423c47600161b061a6d0a7088e
Rik <rnetten@outlook.com>
2023-02-25 12:50:28 +0100

    Created test component

---
+++ worker-thread-test/src/app/worker-test-component/worker-test-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-test-component',
+  templateUrl: './worker-test-component.component.html',
+  styleUrls: ['./worker-test-component.component.css']
+})
+export class WorkerTestComponentComponent {
+
+}
